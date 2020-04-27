16e03de5d8871cf3a7ea1352c007794be27d2eed
bgreen53 <green.barry53@gmail.com>
2020-04-26 20:55:02 -0700

    configured server

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express")
+var path = require("path")
+
+var app = express()
+var PORT = process.env.PORT || 8080
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Add the application routes
+// require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+// require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+// Start listening on PORT
+app.listen(PORT, function() {
+  console.log('Friend Finder app is listening on PORT: ' + PORT);
+});
